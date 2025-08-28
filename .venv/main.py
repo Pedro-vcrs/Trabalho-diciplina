e3612e5ead0d8c826d59fadda1a3d6a90d81435a
Pedro Vinicius <pedro.vcrs1@gmail.com>
2025-08-27 23:09:05 -0300

    fastapi na main.py

---
+++ .venv/main.py
@@ -0,0 +1,16 @@
+from typing import Union
+import random
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/helloworld")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/funcaoteste")
+async def funcaoteste():
+    return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
